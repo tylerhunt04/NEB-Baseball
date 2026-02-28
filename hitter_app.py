8d4be2b1bf9a427e70ffcacaa5d52d612d840c22
Tyler Hunt <tylerhunt2704@gmail.com>
2026-02-28 10:27:33 -0600

    Update DATA_PATH_2026 to use Season2026.csv

--- hitter_app.py
+++ hitter_app.py
@@ -257,7 +257,7 @@ st.markdown("""
 # Default data paths per period
 DATA_PATH_2025   = "B10C25_hitter_app_columns.csv"
 DATA_PATH_SCRIM  = "Scrimmage(27).csv"
-DATA_PATH_2026   = "B10C26_hitter_app_columns.csv"
+DATA_PATH_2026   = "Season2026.csv"
 
 PROB_LOOKUP_PATH = "EV_LA_probabilities.csv"
 

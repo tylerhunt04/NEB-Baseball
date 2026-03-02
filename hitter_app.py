25abf6cf3a2905825f033716f84f78c68a4e05b1
Tyler Hunt <tylerhunt2704@gmail.com>
2026-03-02 16:34:01 -0600

    Update hitter_app.py

--- hitter_app.py
+++ hitter_app.py
@@ -257,7 +257,7 @@ st.markdown("""
 # Default data paths per period
 DATA_PATH_2025   = "B10C25_hitter_app_columns.csv"
 DATA_PATH_SCRIM  = "Scrimmage(27).csv"
-DATA_PATH_2026   = "Neb2026.csv"
+DATA_PATH_2026   = "_Neb2026.csv"
 
 PROB_LOOKUP_PATH = "EV_LA_probabilities.csv"
 
